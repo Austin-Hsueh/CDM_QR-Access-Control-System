74be2271a98f3547c6f57c06836f772f465f1a11
Eric Chiu <c3366301@gmail.com>
2024-08-01 22:20:13 +0800

    安裝 quartz 排程

---
+++ DoorWebApp/ScheduledJob.cs
@@ -0,0 +1,15 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+
+
+public class ScheduledJob : IJob
+{
+    public Task Execute(IJobExecutionContext context)
+    {
+        // 你的任務邏輯
+        Console.WriteLine($"任務執行於: {DateTime.Now}");
+        return Task.CompletedTask;
+    }
+}
+
