70072a8d3bf072484ff29cdb717fbfd15763da4a
Eric Chiu <c3366301@gmail.com>
2024-09-01 22:37:18 +0800

    [HttpGet("v1/User/PermissionSetting/{UserId}")] 回傳 單時段 多時段
    (cherry picked from commit d11ba9f0efde5072b6e27aacf9cefe2cdfe2c783)

---
+++ DoorWebApp/Models/DTO/ManyPermissionsDTO.cs
@@ -0,0 +1,18 @@
+﻿using DoorDB;
+
+namespace DoorWebApp.Models.DTO
+{
+    public class ManyPermissionsDTO
+    {
+        public string datefrom { get; set; } // 權限日期起
+        public string dateto { get; set; } // 權限日期訖
+        public string timefrom { get; set; } // 權限時間起
+        public string timeto { get; set; } // 權限時間訖
+        public List<int> days { get; set; } // 權限一周哪幾天
+        public string qrcode { get; set; } // qrcode
+        public List<int> groupIds { set; get; }
+        public List<ManyTimePermissionDTO> permissions { set; get; }
+        public List<ManyTimePermissionDTO> studentpermissions { set; get; }
+    }
+
+}
